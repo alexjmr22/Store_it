08b5d415fff40cfc4d63d513acbd0300b0ba4277
alexjmr <alexandrejmr96@gmail.com>
2025-08-16 16:07:25 +0100

    first commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AuthForm from '@/components/AuthForm'
+console.log(AuthForm)
+
+const SignIn = () => <AuthForm type="sign-in"/>
+
+export default SignIn;
+
---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="h1">
+        the only storage that you need
+      </h1>
+    </div>
+  );
+}
